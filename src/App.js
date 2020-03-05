614ac1b42ba9b0b70ea1d670c441d0dc553768a5
theFl00f <celeste.ellerby@gmail.com>
2020-03-05 17:06:35 -0500

    firebase imported

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import firebaseApp from './firebaseApp'
+import firebase from './firebase'
 
 class App extends Component {
   render() {
 
     return (
       <div className="App">
-    
+        
       </div>
     )
   }
