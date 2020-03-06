f15860245aec2ed001e93ac6a60a29b16c5a1094
michaelleriche <mikeleriche@gmail.com>
2020-03-05 19:27:15 -0500

    get answer from api

--- src/GeneralSearch.js
+++ src/GeneralSearch.js
@@ -11,27 +11,44 @@ class GeneralSearch extends Component {
    
     componentDidMount() {
         axios({
-            url: `http://api.tvmaze.com/search/shows/`,
+            url: `http://api.tvmaze.com/shows?`,
             method: 'GET',
             params: {
+                page: 'X',
                 language: 'English',
             }
         }).then(response => {
             const tv = response.data;
             this.setState({
+            tv: tv,
             })
             console.log(tv)
+            console.log(this.state.tv.name);
         })
     }
 
     render() {
-  
-      return (
-        <div>
-            <p>dfdfd</p>
+        return (
+            <div>
+                {this.state.tv.map(show => {
+                return (
+                    <div key={show.id} className="tv-titles">
+                        <h2>{`${show.name}`}</h2>
+                        {/* <p>{`${show.summary}`}</p> */}
+                        <p>{`${show.genres}`}</p>
+                        {/* <p>{`${show.network.name}`}</p> */}
+                        <p>Average Rating: {`${show.rating.average}`}</p>
+
+                        
+                        <img src={`${show.image.medium}`} alt=""/>
+                    </div>
+                )
+            })}
         </div>
       )
     }
+
+    
   }
   
   
