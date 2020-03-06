d95ebfa8a8d67376a2a58ef3737e1fcf4b172f12
theFl00f <celeste.ellerby@gmail.com>
2020-03-06 10:29:38 -0500

    firebase failure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
 import firebase from './firebase'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tvShows: []
+    }
+  }
+
+  componentDidMount() {
+    const dbRef = firebase.database().ref();
+    dbRef.on('value', (response) => {
+      console.log(response.val())
+    })
+  }
+
   render() {
 
     return (
       <div className="App">
-        
+        <ul>
+          {this.state.tvShows.map(tvShow => {
+            return <li>{tvShow}</li>
+          })}
+        </ul>
       </div>
     )
   }
