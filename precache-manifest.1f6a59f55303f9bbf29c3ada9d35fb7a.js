5039c0730c4acff6b70685d1fff4f8e44e60b02c
theFl00f <celeste.ellerby@gmail.com>
2020-03-09 14:50:15 -0400

    Updates

---
+++ precache-manifest.1f6a59f55303f9bbf29c3ada9d35fb7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fca35166013ae675f3928c58df359533",
+    "url": "/repo-name/index.html"
+  },
+  {
+    "revision": "ed2932cb450bb161473b",
+    "url": "/repo-name/static/css/main.478448f3.chunk.css"
+  },
+  {
+    "revision": "bf23cb61cc6b28690e8b",
+    "url": "/repo-name/static/js/2.caf64a38.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/repo-name/static/js/2.caf64a38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed2932cb450bb161473b",
+    "url": "/repo-name/static/js/main.81aadd8b.chunk.js"
+  },
+  {
+    "revision": "df9fd5d7f4abe3cdb222",
+    "url": "/repo-name/static/js/runtime-main.71c3e72b.js"
+  }
+]);
