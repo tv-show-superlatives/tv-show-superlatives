536b167af062b932a5195c5e394ff00737fa9b9e
LucasNemeth <l_orion_n@hotmail.com>
2020-03-11 15:42:11 -0400

    starting to add the addToList functionality

--- src/AddToListButton.js
+++ src/AddToListButton.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 class AddToListButton extends Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+        
+    }
+
     render() {
         return (
-            <button className="addToList">add to list</button>
+            <button className="addToList" onClick=
+                {
+                    this.state.userTV.map((show, index) => {
+                        return (
+                            <div key={index} className="searchResults">
+                                <Switch>
+                                    <Link
+                                        to={`/tvShows/${show.show.externals.tvrage}`}
+                                    >
+                                        {show.show.image === null ? (
+                                            <p>No Image!</p>
+                                        ) : (
+                                                <img
+                                                    src={show.show.image.medium}
+                                                    alt={`A poster of ${show.show.name}`}
+                                                    title={`An image of ${show.show.name}`}
+                                                />
+                                            )}
+                                    </Link>
+                                </Switch>
+                                <AddToListButton />
+                            </div>
+                        );
+                    })
+                }>
+                add to list</button>
         )
     }
 }
